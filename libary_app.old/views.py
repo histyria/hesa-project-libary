5f0fc9d634f8a65a62938e99f0334b838aab3e51
nagib alotiby <histyria@gmail.com>
2022-04-22 01:26:02 +0300

    remove app old

--- libary_app.old/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import *
-# Create your views here.
-def index(request):
-    book = Book.objects.all()
-    return render(request, 'index.html' , {'book':book})
